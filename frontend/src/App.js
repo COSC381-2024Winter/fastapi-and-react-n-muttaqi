0754061ddd4e92e3499496329307903bdaafa171
Turjo Muttaqi <nmuttaqi@emich.edu>
2024-04-16 20:39:14 -0400

    Implement movie fetching from FastAPI based on movie_id
    Added functionality to fetch movie details from FastAPI.
    
    Issue: #7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
-import './App.css'; // Import your CSS file
+import './App.css'; 
 
 function App() {
   const [movieId, setMovieId] = useState('');
+  const [movieDetails, setMovieDetails] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleMovieIdChange = (event) => {
     setMovieId(event.target.value);
   };
+
+  useEffect(() => {
+    console.log(movieId);
+    if (movieId.trim() !== '') {
+      fetch(`http://localhost:8000/movies/${movieId}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Movie not found');
+          }
+          return response.json();
+        })
+        .then(data => {
+          setMovieDetails(data);
+          //console.log(data)
+          setError(null);
+        })
+        .catch(error => {
+          setMovieDetails(null);
+          setError('Movie not found');
+        });
+    } else {
+      setMovieDetails(null);
+      setError(null);
+    }
+  }, [movieId]);
 
   return (
     <div className="App">
@@ -20,9 +47,10 @@ function App() {
           className="TextField"
         />
       </div>
-      <p>Movie ID: {movieId}</p>
     </div>
   );
 }
 
 export default App;
+
+
